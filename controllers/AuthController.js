837ca3cdbb17c549608ae8b878506f75c1426664
nnodim <nnodimobinna@gmail.com>
2023-05-31 00:52:41 +0100

    4. Authenticate a user

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,14 +4,14 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AuthController {
-  static async getConnect(request, response) {
-    const authData = request.header('Authorization');
+  static async getConnect(req, res) {
+    const authData = req.header('Authorization');
     let userEmail = authData.split(' ')[1];
     const buff = Buffer.from(userEmail, 'base64');
     userEmail = buff.toString('ascii');
     const data = userEmail.split(':'); // contains email and password
     if (data.length !== 2) {
-      response.status(401).json({ error: 'Unauthorized' });
+      res.status(401).json({ error: 'Unauthorized' });
       return;
     }
     const hashedPassword = sha1(data[1]);
@@ -21,22 +21,22 @@ class AuthController {
         const token = uuidv4();
         const key = `auth_${token}`;
         await redisClient.set(key, user._id.toString(), 60 * 60 * 24);
-        response.status(200).json({ token });
+        res.status(200).json({ token });
       } else {
-        response.status(401).json({ error: 'Unauthorized' });
+        res.status(401).json({ error: 'Unauthorized' });
       }
     });
   }
 
-  static async getDisconnect(request, response) {
-    const token = request.header('X-Token');
-    const key = `auth_${token}`;
-    const id = await redisClient.get(key);
+  static async getDisconnect(req, res) {
+    const token = req.header('X-Token');
+    const authKey = `auth_${token}`;
+    const id = await redisClient.get(authKey);
     if (id) {
-      await redisClient.del(key);
-      response.status(204).json({});
+      await redisClient.del(authKey);
+      res.status(204).json({});
     } else {
-      response.status(401).json({ error: 'Unauthorized' });
+      res.status(401).json({ error: 'Unauthorized' });
     }
   }
 }
